67747f0b8fd080c91102e68b7fecddd772b0cd40
nwnbasnayake <97802730+nwnbasnayake@users.noreply.github.com>
2023-07-13 21:55:10 +0530

    uplord app code

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\NSBM Green University\\3rd year 1st Semester\\firstdemoapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\NSBM Green University\\3rd year 1st Semester\\firstdemoapp"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\NSBM Green University\\3rd year 1st Semester\\firstdemoapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\NSBM Green University\\3rd year 1st Semester\\firstdemoapp"
+  "FLUTTER_TARGET=D:\\NSBM Green University\\3rd year 1st Semester\\firstdemoapp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80NWY2ZTAwOTExMGRmNGYzNGVjMmNmOTlmNjNjZjczYjcxYjdhNDIwLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\NSBM Green University\\3rd year 1st Semester\\firstdemoapp\\.dart_tool\\package_config.json"
+)
